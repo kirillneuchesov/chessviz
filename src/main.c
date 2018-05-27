073a80fe18d57605180cb6ae651ddb644a382110
kirillneuchesov <36182439+kirillneuchesov@users.noreply.github.com>
2018-05-27 20:31:00 +0700

    Fix for tests main.c

--- src/main.c
+++ src/main.c
@@ -1,44 +1,99 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "p.h"
+#include "P.h"
 #include "board.h"
-#include <locale.h>
+#include "horse.h"
+#include "king.h"
+#include "el.h"
+#include "queen.h"
+#include "rook.h"
 
 int main()
 {
-	setlocale(LC_ALL, "Rus");
-    system("cls");
-    int i = 0,ext;
-    char places[6];
-    
-    while ( i == 0) {
-        char q;
-        printf("Для начала нажмите (S) для выхода (Q)\n");
-        scanf("%c", &q);
-        
-        if (q == 's') {
-            system("cls");
-		    print_new_board();
-            while (ext == 0) {
-                printf("Введите координаты или нажмите (Q) для выхода:\n" );
-                scanf("%s", places);
-                if (places[0] == 'q') {
-					system("cls");
-					printf("Игра окончена\n");
-                    return 0;
-                } else {
-                    ext = board_func(places, 0);
-                    if (ext == -1) {
-                        printf("Неверные координаты\n");
-                        ext = 0;
-                    }
-                }
-            }
-        } else if (q == 'q') {
-            system("cls");
-            printf("Игра окончена\n");
-            return 0;
-        }
+    system("clear");
+    char s[9][9], k[5];
+    int z1 = 0, z2 = 0, z3 = 0, z4 = 0, z5 = 0, z6 = 0, z7 = 0, z8 = 0, z9 = 0, z10 = 0, 
+    z11 = 0, z12 = 0, z13 = 0, z14 = 0, flag1 = 0;
+
+    fillboard(s);  
+    board(s);
+
+while (1){
+    if (flag1 == 0) printf("РњР°Р»С‹Рµ\n");
+    else if (flag1 == 1) printf("Р‘РѕР»СЊС€РёРµ\n");
+    for(int i = 0;i <= 5;i++) {
+    k[i]=getchar();
     }
-    return 0;
+
+    system("clear");
+
+    if (flag1 == 0) {
+    z1 = pmove(k,s);
+    z2 = phack(k,s);
+    z5 = rook(k,s);
+    z7 = horse(k,s);
+    z9 = el(k,s);
+    z11 = king(k,s);
+    z13 = queen(k, s);
+    if ((z1 == 1) && (z2 == 1) && (z5 == 1) && (z7 == 1) && (z9 == 1) && (z11 == 1) 
+    && (z13 == 1)) {
+    system("clear");
+    printf("РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ С…РѕРґ\n"); 
+    z1 = 0; 
+    z2 = 0;
+    z5 = 0; 
+    z7 = 0; 
+    z9 = 0; 
+    z11 = 0;
+    z13 = 0;
+}
+    else {
+    z1 = 0; 
+    z2 = 0; 
+    z5 = 0; 
+    z7 = 0; 
+    z9 = 0; 
+    z11 = 0;
+    z13 = 0; 
+    flag1=1;
+}
 }
 
+    else if (flag1 == 1)
+{
+    z3 = Pmove(k,s);
+    z4 = Phack(k,s);
+    z6 = Rook(k,s);
+    z8 = Horse(k,s);
+    z10 = El(k,s);
+    z12 = King(k,s);
+    z14 = Queen(k,s);
+    if ((z3 == 1) && (z4 == 1) && (z6 == 1) && (z8 == 1) && (z10 == 1) && (z12 == 1) 
+    && (z14 == 1)) { 
+    system("clear");
+    printf("РќРµРїСЂР°РІРёР»СЊРЅС‹Р№ С…РѕРґ\n"); 
+    z3 = 0; 
+    z4 = 0;
+    z6 = 0; 
+    z8 = 0; 
+    z10 = 0; 
+    z12 = 0; 
+    z14 = 0;
+}
+    else {
+    z3 = 0; 
+    z4 = 0;
+    z6 = 0; 
+    z8 = 0; 
+    z10 = 0; 
+    z12 = 0; 
+    z14 = 0; 
+    flag1=0;
+}
+}
+
+    board(s);
+}
+    return 0;
+}
